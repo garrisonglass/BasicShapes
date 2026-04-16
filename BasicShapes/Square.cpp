5da0c0b41d9c2df3b579abbfafb259601cef9804
garrisonglass <garrison.glass@gmail.com>
2026-04-15 21:48:53 -0400

    added square.cpp

--- BasicShapes/Square.cpp
+++ BasicShapes/Square.cpp
@@ -1,1 +1,14 @@
 #include "Square.h"
+
+Square::Square(double s, const string& n) : Rectangle(s, s, n), side(s) 
+{
+	setName(n);
+}
+
+void Square::setSide(double s)
+{
+	side = s;
+	//Updates dimensions
+	setLength(s);
+	setWidth(s);
+}
