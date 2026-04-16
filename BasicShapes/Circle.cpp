1976c611178657dd6cbbb58afeca6143f595241c
garrisonglass <garrison.glass@gmail.com>
2026-04-15 21:24:14 -0400

    Added Circle.cpp

--- BasicShapes/Circle.cpp
+++ BasicShapes/Circle.cpp
@@ -1,2 +1,30 @@
 #include "Circle.h"
 #include <cmath>
+
+Circle::Circle(double x, double y, double r, const string& n) : xCenter(x), yCenter(y), radius(r)
+{
+	setName(n);
+	calcArea();//Calculate area when the circle is created
+}
+
+void Circle::setX(double x)
+{
+	xCenter = x;
+}
+
+void Circle::setY(double y)
+{
+	yCenter = y;
+}
+
+void Circle::setR(double r)
+{
+	radius = r;
+	calcArea();//Recalculate area whenever the radius is updated
+}
+
+void Circle::calcArea()
+{
+	double a = 3.14159 * radius * radius;
+	setArea(a);
+}
