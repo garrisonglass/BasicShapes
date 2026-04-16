7d83599a0769f2c95cb73f1ba20c62527ddf15d8
garrisonglass <garrison.glass@gmail.com>
2026-04-15 21:46:35 -0400

    added Square.h

---
+++ BasicShapes/Square.h
@@ -0,0 +1,19 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include "Rectangle.h"
+
+class Square : public Rectangle
+{
+private:
+		double side;
+public:
+	Square(double s, const string& n = "Square");
+
+	// Getter
+	double getSide() const { return side; }
+	// Setter
+	void setSide(double s);
+	
+};
+#endif
