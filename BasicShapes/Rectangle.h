ff99a016766bcdce10fa5e8583a0ff695c06a704
garrisonglass <garrison.glass@gmail.com>
2026-04-15 21:33:57 -0400

    Added Rectangle.h

---
+++ BasicShapes/Rectangle.h
@@ -0,0 +1,23 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include "BasicShape.h"
+
+class Rectangle : public BasicShape
+{
+private:
+	double length;
+	double width;
+public:
+	Rectangle(double l, double w, const string& n = "Rectangle");
+
+	// Getters
+	double getLength() const { return length; }
+	double getWidth() const { return width; }
+	// Setters
+	void setLength(double l) { length = l; calcArea(); }//Recalculate area when length is updated
+	void setWidth(double w) { width = w; calcArea(); }//Recalculate area when width is updated
+	//Override the calcArea method to calculate the area of the rectangle
+	void calcArea() override
+};
+#endif
